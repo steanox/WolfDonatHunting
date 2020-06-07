6c45b9bc43a9c40eec1e6129dadaa5f2135c24d2
David Gunawan <david_gunawan_@hotmail.com>
2020-06-07 21:52:01 +0700

    Remove SetActive Camera
    There's no need to change camera perspective, since we don't have any camera in level1scene

--- 3DGameSceneKit/3D Wolf/Scene/Level1Scene.swift
+++ 3DGameSceneKit/3D Wolf/Scene/Level1Scene.swift
@@ -150,7 +150,6 @@ class Level1Scene: SCNScene {
         self.cameraNode.camera!.zFar = 200
         self.rootNode.addChildNode(cameraNode)
 
-        setActiveCamera("camLookAt_cameraGame", animationDuration: 0.0)
     }
     
     func setupFollowCamera(_ cameraNode: SCNNode) {
@@ -216,58 +215,6 @@ class Level1Scene: SCNScene {
 
         cameraNode.constraints = [follow, keepAltitude, accelerationConstraint, orientationUpdateConstraint, lookAtConstraint]
     }
-    
-    func setActiveCamera(_ cameraName: String, animationDuration duration: CFTimeInterval) {
-        guard let camera = self.rootNode.childNode(withName: cameraName, recursively: true) else { return }
-        if self.activeCamera == camera {
-            return
-        }
-
-        self.lastActiveCamera = activeCamera
-        if activeCamera != nil {
-            self.lastActiveCameraFrontDirection = (activeCamera?.presentation.simdWorldFront)!
-        }
-        self.activeCamera = camera
-
-        // save old transform in world space
-        let oldTransform: SCNMatrix4 = cameraNode.presentation.worldTransform
-
-        // re-parent
-        camera.addChildNode(cameraNode)
-
-        // compute the old transform relative to our new parent node (yeah this is the complex part)
-        let parentTransform = camera.presentation.worldTransform
-        let parentInv = SCNMatrix4Invert(parentTransform)
-
-        // with this new transform our position is unchanged in workd space (i.e we did re-parent but didn't move).
-        cameraNode.transform = SCNMatrix4Mult(oldTransform, parentInv)
-
-        // now animate the transform to identity to smoothly move to the new desired position
-        SCNTransaction.begin()
-        SCNTransaction.animationDuration = duration
-        SCNTransaction.animationTimingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
-        cameraNode.transform = SCNMatrix4Identity
-
-        if let cameraTemplate = camera.camera {
-            cameraNode.camera!.fieldOfView = cameraTemplate.fieldOfView
-            cameraNode.camera!.wantsDepthOfField = cameraTemplate.wantsDepthOfField
-            cameraNode.camera!.sensorHeight = cameraTemplate.sensorHeight
-            cameraNode.camera!.fStop = cameraTemplate.fStop
-            cameraNode.camera!.focusDistance = cameraTemplate.focusDistance
-            cameraNode.camera!.bloomIntensity = cameraTemplate.bloomIntensity
-            cameraNode.camera!.bloomThreshold = cameraTemplate.bloomThreshold
-            cameraNode.camera!.bloomBlurRadius = cameraTemplate.bloomBlurRadius
-            cameraNode.camera!.wantsHDR = cameraTemplate.wantsHDR
-            cameraNode.camera!.wantsExposureAdaptation = cameraTemplate.wantsExposureAdaptation
-            cameraNode.camera!.vignettingPower = cameraTemplate.vignettingPower
-            cameraNode.camera!.vignettingIntensity = cameraTemplate.vignettingIntensity
-        }
-        SCNTransaction.commit()
-    }
-
-    func setActiveCamera(_ cameraName: String) {
-        setActiveCamera(cameraName, animationDuration: Level1Scene.DefaultCameraTransitionDuration)
-    }
 }
 
 extension Level1Scene: SCNPhysicsContactDelegate{
@@ -276,8 +223,6 @@ extension Level1Scene: SCNPhysicsContactDelegate{
             collision(withRing: ring)
         }
     }
-    
-    
 }
 
 
